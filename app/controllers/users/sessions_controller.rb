6bd04b1a79055fc9a021fd8e1c68f2e29d0523f4
jane shin <rmhg5140@naver.com>
2018-07-02 08:41:37 +0000

    180702

---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+class Users::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super # 부모에 있는거 그대로 가져오는것.
+  # end
+# 메소드를 오버라이드함으로써 우리가 원하는 로직을 사용가능.
+
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+  # protected
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
